a65588942adddaee30c8634914faf7c94ed5e60f
Youyan WU <wuyouyan1@gmail.com>
2019-11-15 23:12:42 +0100

    HTTP
    Signed-off-by: Youyan WU <wuyouyan1@gmail.com>

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private  searchTerms = new Subject<string>(); // A Subject is both a source of observable values and an Observable itself.
+                                                // You can subscribe to a Subject as you would any Observable.
+
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      // waiting 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      /*switchMap() preserves the original request order
+      while returning only the observable from the most recent HTTP method call.
+      Results from prior calls are canceled and discarded.*/
+      // switch to new search observable each time the term changes
+      switchMap((term: string) =>
+        this.heroService.searchHeroes(term)),
+    );
+  }
+}
